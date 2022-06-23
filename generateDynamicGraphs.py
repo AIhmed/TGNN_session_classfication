d06d2021011e925d03fde4e5998caa25fd88224f
AIhmed <bouliche.ahmed.2@gmail.com>
2022-06-23 12:03:38 +0100

    changed generation algorithm

--- generateDynamicGraphs.py
+++ generateDynamicGraphs.py
@@ -2,9 +2,10 @@ import pandas as pd
 import random
 from transformers import AutoTokenizer
 from torch.nn import Embedding
-import numpy as np
+#  import torch
+#  import numpy as np
 import json
-from torch_geometric_temporal import DynamicGraphTemporalSignal
+#  from torch_geometric_temporal import DynamicGraphTemporalSignal
 EMBEDDING_SIZE = 768
 
 df = pd.DataFrame(pd.read_excel('labeledDataset.xlsx'))
@@ -35,7 +36,7 @@ def max_comment_len(df):
 sequence_lengths, embedding_space, labels = max_comment_len(df)
 node_features = [[[] for timesteps in range(max(sequence_lengths.values()))] for session in range(len(df)//10)]
 targets = [[[] for timestep in range(max(sequence_lengths.values()))] for session in range(len(df)//10)]
-g = [[[[] for j in range(2)] for seq in range(max(sequence_lengths.values()))] for session in range(len(df) // 10)]
+edge_indices = [[[[] for j in range(2)] for seq in range(max(sequence_lengths.values()))] for session in range(len(df) // 10)]
 edge_weights = [[[] for timesteps in range(max(sequence_lengths.values()))] for session in range(len(df)//10)]
 
 
@@ -53,32 +54,45 @@ def create_temporal_graph_example(session, start, session_len):
     for i in range(session_len):
         con = random.randint(1, session_len)
         if i != con and i != owner:
-            if con in g[session][0][0]:
-                ind = g[session][0][0].index(con)
-                if g[session][0][1][ind] != i:
-                    g[session][0][0].append(i)
-                    g[session][0][1].append(con)
+            if con in edge_indices[session][0][0]:
+                ind = edge_indices[session][0][0].index(con)
+                if edge_indices[session][0][1][ind] != i:
+                    edge_indices[session][0][0].append(i)
+                    edge_indices[session][0][1].append(con)
                     attr_edge_weight(session, 0, start+i, start+con)
                 else:
                     continue
             else:
-                g[session][0][0].append(i)
-                g[session][0][1].append(con)
+                edge_indices[session][0][0].append(i)
+                edge_indices[session][0][1].append(con)
                 attr_edge_weight(session, 0, start+i, start+con)
-        node_features[session][0].append(embedding_space[start+i][0])
-        targets[session][0].append(labels[start + i])
+    node_set = set(edge_indices[session][0][0]).union(set(edge_indices[session][0][1]))
+    print("the set of nodes we have in this session")
+    print(node_set)
+    for node in node_set:
+        node_features[session][0].append(embedding_space[start + node][0])
+        targets[session][0].append(labels[start + node])
 
     for t in range(1, max(list(sequence_lengths.values())[start: start+session_len])):
         print(f"temporal graph number {session} and time step number {t}")
         for i in range(session_len):
-            targets[session][t].append(labels[start + i])
             print(f"the comment number {i} in the current session")
-            if len(embedding_space[start + i]) > t:
-                node_features[session][t].append(embedding_space[start + i][t])
-                if i < len(edge_weights[session][0]):
-                    edge_weights[session][t].append(edge_weights[session][0][i])
-                    g[session][t][0].append(g[session][0][0][i])
-                    g[session][t][1].append(g[session][0][1][i])
+            if len(embedding_space[start + i]) > t:  #  this is to loop through each node in the graph
+                #  add the token corresponding to the comment and the time step if the comment is too long
+                if i in edge_indices[session][0][0]:  #  if a connection with this node exist create one for this timestep
+                    edge_indices[session][t][0].append(i)
+                    index = edge_indices[session][t][0].index(i)  #  get index of the node i in the first graph
+                    edge_indices[session][t][1].append(edge_indices[session][0][1][index])  # find connection of i
+                    edge_weights[session][t].append(edge_weights[session][0][index])
+                #  now the problem is with node that only receive an edge, they will be added in the node lists
+
+        node_set = set(edge_indices[session][t][0]).union(set(edge_indices[session][t][1]))
+        print(f"the set of node existing in the session are")
+        print(node_set)
+        for node in node_set:
+            if len(embedding_space[start + node]) > t:
+                node_features[session][t].append(embedding_space[start + node][t])
+                targets[session][t].append(labels[start + node])
 
 
 start = 0
@@ -91,12 +105,12 @@ while start < len(df)-20:
 num_el = len(node_features)
 
 for i in range(num_el):
-        if node_features[-1] == [[] for time_step in range(max(sequence_lengths.values()))]:
-            del node_features[-1]
-        if g[-1] == [[[] for j in range(2)] for seq in range(max(sequence_lengths.values()))]:
-            del g[-1]
-        if edge_weights[-1] == [[] for time_step in range(max(sequence_lengths.values()))]:
-            del edge_weights[-1]
+    if node_features[-1] == [[] for time_step in range(max(sequence_lengths.values()))]:
+        del node_features[-1]
+    if edge_indices[-1] == [[[] for j in range(2)] for seq in range(max(sequence_lengths.values()))]:
+        del edge_indices[-1]
+    if edge_weights[-1] == [[] for time_step in range(max(sequence_lengths.values()))]:
+        del edge_weights[-1]
 
 sample_data = list()
 for i in range(len(node_features)):
@@ -104,11 +118,9 @@ for i in range(len(node_features)):
         node_features[i].remove([])
         targets[i].remove([])
         edge_weights[i].remove([])
-        g[i].remove([[], []])
+        edge_indices[i].remove([[], []])
+    sample_data.append({"session": i, "node_features": node_features[i], "edge_indices": edge_indices[i], "edge_weights": edge_weights[i], "targets": targets[i]})
 
-
-for s in node_features:
-    for t in s:
-        print(t)
-data = DynamicGraphTemporalSignal(edge_indices=g[0], edge_weights=edge_weights[0], features=node_features[0], targets=targets[0])
-example = next(iter(data))
+f = open("sample_data1.json", "w")
+json.dump(sample_data, f)
+f.close()
---
+++ session_classification.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch_geometric.nn as gnn
+import json
+VOCAB_SIZE = 64000
+EMBEDDING_SIZE = 256
+
+
+class SpatialEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = nn.Embedding(VOCAB_SIZE, EMBEDDING_SIZE)
+        self.gcn1 = gnn.GCNConv(in_channels=EMBEDDING_SIZE, out_channels=EMBEDDING_SIZE)
+        self.gcn2 = gnn.GCNConv(in_channels=EMBEDDING_SIZE, out_channels=EMBEDDING_SIZE)
+        self.gcn3 = gnn.GCNConv(in_channels=EMBEDDING_SIZE, out_channels=EMBEDDING_SIZE)
+
+    def forward(self, node_features, edge_indices, edge_weights):
+        edge_index0 = torch.tensor(edge_indices[0])
+        edge_weight0 = torch.tensor(edge_weights[0], dtype=torch.float32)
+        edge_index1 = torch.tensor(edge_indices[1])
+        edge_weight1 = torch.tensor(edge_weights[1], dtype=torch.float32)
+
+        first = self.embedding(torch.tensor(node_features[0]))
+        first = self.gcn1(x=first, edge_index=edge_index0, edge_weight=edge_weight0)
+        first = self.gcn2(x=first, edge_index=edge_index0, edge_weight=edge_weight0)
+        first = self.gcn3(x=first, edge_index=edge_index0, edge_weight=edge_weight0)
+
+        second = self.embedding(torch.tensor(node_features[1]))
+        second = self.gcn1(x=second, edge_index=edge_index1, edge_weight=edge_weight1)
+        second = self.gcn2(x=second, edge_index=edge_index1, edge_weight=edge_weight1)
+        second = self.gcn3(x=second, edge_index=edge_index1, edge_weight=edge_weight1)
+
+        t = torch.cat((first, second))
+        for i in range(2, len(node_features)):
+            print("\n\n\n")
+            i_edge_index = torch.tensor(edge_indices[i])
+            i_edge_weight = torch.tensor(edge_weights[i], dtype=torch.float32)
+            nth = self.embedding(torch.tensor(node_features[i]))
+            nth = self.gcn1(x=nth, edge_index=i_edge_index, edge_weight=i_edge_weight)
+            nth = self.gcn2(x=nth, edge_index=i_edge_index, edge_weight=i_edge_weight)
+            nth = self.gcn3(x=nth, edge_index=i_edge_index, edge_weight=i_edge_weight)
+            t = torch.cat((t, nth))
+            print(t.shape)
+        return t
+
+
+f = open("sample_data1.json", "r")
+data = json.load(f)
+f.close()
+spatial = SpatialEncoding()
+num_tokens = sum([len(timestep) for timestep in data[0]['node_features']])
+print(num_tokens)
+out = spatial(data[0]['node_features'], data[0]['edge_indices'], data[0]['edge_weights'])
+print(out.shape)
